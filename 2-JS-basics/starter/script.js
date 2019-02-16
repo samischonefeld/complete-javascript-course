54fb7782b6955096e1630fd8ef8f060fc56e4f37
Samantha Schonefeld <samanthaschonefeld@Samis-MacBook-Pro.local>
2019-02-15 16:53:22 -0800

    first lecture

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,17 @@
+var firstName = 'Sami';
+
+console.log(firstName)
+
+var lastName = 'Schonefeld';
+var age = 25;
+
+var fullAge = true;
+console.log(fullAge)
+
+var job;
+
+job = ('teacher')
+
+console.log(job);
+
+
